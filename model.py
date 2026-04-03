bcc16b2e61f4d630045742df737fbe6f9d37883b
saniya sonule <saniyasonule@gmail.com>
2026-04-04 00:57:23 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("spam.csv", encoding='latin-1')
+
+# Automatically pick first two columns
+df = df.iloc[:, :2]
+df.columns = ['label', 'message']
+ 
+# Convert labels to numbers
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    df['message'], df['label'], test_size=0.2, random_state=42
+)
+
+# Convert text to numbers
+vectorizer = TfidfVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+# Train model
+model = LogisticRegression()
+model.fit(X_train_vec, y_train)
+
+# Predict
+y_pred = model.predict(X_test_vec)
+
+# Accuracy
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1 Score:", f1_score(y_test, y_pred))
+
+# Save objects
+import pickle
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
